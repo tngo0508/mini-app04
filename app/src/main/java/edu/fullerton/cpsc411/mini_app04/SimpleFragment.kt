ebbeb86a36dde84742fc892fb6df4c2b97738219
thomas <tngo0508@gmail.com>
2018-12-03 23:31:09 -0800

    add data binding and update the transfer time

--- app/src/main/java/edu/fullerton/cpsc411/mini_app04/SimpleFragment.kt
+++ app/src/main/java/edu/fullerton/cpsc411/mini_app04/SimpleFragment.kt
@@ -8,7 +8,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import edu.fullerton.cpsc411.mini_app04.databinding.FragmentSimpleBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_simple.view.*
 import java.util.Locale.US
 
@@ -27,76 +33,23 @@ private const val RESULT = "result"
  *
  */
 class SimpleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var netSpeed: Double = 0.0
-//    private var fileSize: Double = 0.0
-//    private var result: String? = null
-//    private var listener: OnFragmentInteractionListener? = null
+
+    // Obtain ViewModel from ViewModelProviders
+    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            it.putDouble(NET_WORK, netSpeed)
-//            it.putDouble(FILE_SIZE, fileSize)
-//            it.putString(RESULT, result)
-//        }
         retainInstance = true
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_simple, container, false)
-        val fileSizeInput = rootView.FileSizeInput
-        val networkSpeedInput = rootView.NetworkSpeedInput
-        val resultView = rootView.Result
 
-        val defaultListener = object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
+        val binding:FragmentSimpleBinding =
+                DataBindingUtil.inflate(layoutInflater, R.layout.fragment_simple, container, false)
+        binding.viewmodel = viewModel
 
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val notEmpty: TextView.() -> Boolean = { text.isNotEmpty() }
-
-                if (notEmpty(fileSizeInput) and notEmpty(networkSpeedInput)) {
-                    val speed = java.lang.Double.valueOf(networkSpeedInput.text.toString())
-//                    netSpeed = speed
-                    val size = java.lang.Double.valueOf(fileSizeInput.text.toString())
-//                    fileSize = size
-
-                    if (size == 0.0) {
-                        resultView.text = getString(R.string.invalid_input)
-//                        result = resultView.text.toString()
-                    } else {
-                        val output = resultView.toOneDec(resultView.cal(speed, size)) + " seconds"
-                        resultView.text = output
-//                        result = output
-                    }
-                } else {
-                    val time = resultView.toOneDec(resultView.cal(100.0, 100.0)) + " seconds"
-                    resultView.text = time
-//                    result = time
-                }
-            }
-
-        }
-        networkSpeedInput.addTextChangedListener(defaultListener)
-        fileSizeInput.addTextChangedListener(defaultListener)
-
-        return rootView
-    }
-
-    private fun TextView.cal(networkSpeed: Double?, fileSize: Double?): Double {
-        return networkSpeed!! * Math.pow(2.0, 20.0) * 8.0 / (fileSize!! * Math.pow(10.0, 6.0))
-    }
-
-    private fun TextView.toOneDec(num: Double?): String {
-        return String.format(US, "%.1f", num)
+        return binding.root
     }
 
     companion object {
--- app/src/main/java/edu/fullerton/cpsc411/mini_app04/SimpleViewModel.kt
+++ app/src/main/java/edu/fullerton/cpsc411/mini_app04/SimpleViewModel.kt
@@ -1,14 +1,47 @@
 package edu.fullerton.cpsc411.mini_app04
 
+import android.widget.TextView
 import androidx.databinding.ObservableDouble
 import androidx.lifecycle.ViewModel
+import java.util.Locale.US
 
 class SimpleViewModel: ViewModel() {
-    val netSpeed = 0.0
-    val fileSize = 0.0
+    var netSpeed = 100.0
+    var fileSize = 100.0
     val transferTime = ObservableDouble()
 
-    fun onTextChange() {
-        transferTime.set(netSpeed * Math.pow(2.0, 20.0) * 8.0 / (fileSize * Math.pow(10.0, 6.0)))
+    fun onNetSpeedChanged(s: CharSequence) {
+
+        try {
+            netSpeed = s.toString().toDouble()
+
+            transferTime.set(toOneDec(calTransferTime(netSpeed, fileSize)).toDouble())
+        } catch (e: NumberFormatException){
+            defaultDisplay()
+        }
+
+    }
+
+    fun onFileSizeChanged(s: CharSequence) {
+
+        try {
+            fileSize = s.toString().toDouble()
+
+            transferTime.set(toOneDec(calTransferTime(netSpeed, fileSize)).toDouble())
+        } catch (e: NumberFormatException) {
+            defaultDisplay()
+        }
+
+    }
+    private fun calTransferTime(networkSpeed: Double?, fileSize: Double?): Double {
+        return networkSpeed!! * Math.pow(2.0, 20.0) * 8.0 / (fileSize!! * Math.pow(10.0, 6.0))
+    }
+
+    private fun toOneDec(num: Double?): String {
+        return String.format(US, "%.1f", num)
+    }
+
+    private fun defaultDisplay() {
+        transferTime.set(toOneDec(calTransferTime(100.0, 100.0)).toDouble())
     }
 }
